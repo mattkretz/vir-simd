ce591fb936dcb8c68d43b25e66c1ffe76ace4ddf
Matthias Kretz <m.kretz@gsi.de>
2024-08-23 10:15:34 +0200

    Change copyright holder to GSI
    This code was developed by me while employed at GSI, contributed to GCC
    by me and modified/maintained by me, and finally extracted back for
    vir-simd.
    
    ChangeLog:
    
    	* testsuite/standard_abi_usable.cc: Change Copyright to GSI/me
    	and use SPDX-License-Identifier for GPL3+.
    	* testsuite/tests/abs.cc: Likewise.
    	* testsuite/tests/algorithms.cc: Likewise.
    	* testsuite/tests/bits/conversions.h: Likewise.
    	* testsuite/tests/bits/floathelpers.h: Likewise.
    	* testsuite/tests/bits/main.h: Likewise.
    	* testsuite/tests/bits/make_vec.h: Likewise.
    	* testsuite/tests/bits/mathreference.h: Likewise.
    	* testsuite/tests/bits/metahelpers.h: Likewise.
    	* testsuite/tests/bits/numeric_traits.h: Likewise.
    	* testsuite/tests/bits/simd_view.h: Likewise.
    	* testsuite/tests/bits/test_values.h: Likewise.
    	* testsuite/tests/bits/ulp.h: Likewise.
    	* testsuite/tests/bits/verify.h: Likewise.
    	* testsuite/tests/broadcast.cc: Likewise.
    	* testsuite/tests/casts.cc: Likewise.
    	* testsuite/tests/fpclassify.cc: Likewise.
    	* testsuite/tests/frexp.cc: Likewise.
    	* testsuite/tests/generator.cc: Likewise.
    	* testsuite/tests/hypot3_fma.cc: Likewise.
    	* testsuite/tests/integer_operators.cc: Likewise.
    	* testsuite/tests/ldexp_scalbn_scalbln_modf.cc: Likewise.
    	* testsuite/tests/loadstore.cc: Likewise.
    	* testsuite/tests/logarithm.cc: Likewise.
    	* testsuite/tests/mask_broadcast.cc: Likewise.
    	* testsuite/tests/mask_conversions.cc: Likewise.
    	* testsuite/tests/mask_implicit_cvt.cc: Likewise.
    	* testsuite/tests/mask_loadstore.cc: Likewise.
    	* testsuite/tests/mask_operator_cvt.cc: Likewise.
    	* testsuite/tests/mask_operators.cc: Likewise.
    	* testsuite/tests/mask_reductions.cc: Likewise.
    	* testsuite/tests/math_1arg.cc: Likewise.
    	* testsuite/tests/math_2arg.cc: Likewise.
    	* testsuite/tests/operator_cvt.cc: Likewise.
    	* testsuite/tests/operators.cc: Likewise.
    	* testsuite/tests/reductions.cc: Likewise.
    	* testsuite/tests/remqo.cc: Likewise.
    	* testsuite/tests/simd.cc: Likewise.
    	* testsuite/tests/sincos.cc: Likewise.
    	* testsuite/tests/split_concat.cc: Likewise.
    	* testsuite/tests/splits.cc: Likewise.
    	* testsuite/tests/trigonometric.cc: Likewise.
    	* testsuite/tests/trunc_ceil_floor.cc: Likewise.
    	* testsuite/tests/where.cc: Likewise.

--- testsuite/tests/bits/numeric_traits.h
+++ testsuite/tests/bits/numeric_traits.h
@@ -1,26 +1,9 @@
 // Definition of numeric_limits replacement traits P1841R1 -*- C++ -*-
 
-// Copyright (C) 2020-2022 Free Software Foundation, Inc.
-//
-// This file is part of the GNU ISO C++ Library.  This library is free
-// software; you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 3, or (at your option)
-// any later version.
-
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// Under Section 7 of GPL version 3, you are granted additional
-// permissions described in the GCC Runtime Library Exception, version
-// 3.1, as published by the Free Software Foundation.
-
-// You should have received a copy of the GNU General Public License and
-// a copy of the GCC Runtime Library Exception along with this program;
-// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
-// <http://www.gnu.org/licenses/>.
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright © 2020–2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+ *                       Matthias Kretz <m.kretz@gsi.de>
+ */
 
 #ifndef VIR_NUMERIC_TRAITS_H_
 #define VIR_NUMERIC_TRAITS_H_
