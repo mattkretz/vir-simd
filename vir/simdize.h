mattkretz/vir-simd

commit fd92e08ab4f6a1c06fa7290267515b47244bdafd

Disable simdize with Clang <= 13 because Clang ICEs

vir/simdize.h
@@ -9,7 +9,8 @@
 #include "struct_reflect.h"
 #include "constexpr_wrapper.h"
 
-#if VIR_HAVE_STRUCT_REFLECT and VIR_HAVE_CONSTEXPR_WRAPPER
+#if VIR_HAVE_STRUCT_REFLECT and VIR_HAVE_CONSTEXPR_WRAPPER \
+  and (not defined __clang__ or __clang__ > 13)
 #define VIR_HAVE_SIMDIZE 1
 
 #include <tuple>

commit 7e186058318afac4c56cb921d852e9c9e2589889

Fix Clang identification
The issue was introduced with
commit fd92e08ab4f6a1c06fa7290267515b47244bdafd
    Disable simdize with Clang <= 13 because Clang ICEs

ChangeLog:

	* vir/simdize.h: Change __clang__ to __clang_major__.

vir/simdize.h
@@ -10,7 +10,7 @@
 #include "constexpr_wrapper.h"
 
 #if VIR_HAVE_STRUCT_REFLECT and VIR_HAVE_CONSTEXPR_WRAPPER \
-  and (not defined __clang__ or __clang__ > 13)
+  and (not defined __clang_major__ or __clang_major__ > 13)
 #define VIR_HAVE_SIMDIZE 1
 
 #include <tuple>